0a8e1d89acbfe6d95bf16d0f700fc79dfddc5db1
vinitus <tlsdnrng@gmail.com>
2023-08-03 15:15:55 +0900

    재밌다 웹팩

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+};
