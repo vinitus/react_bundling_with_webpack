b87db3e8d83167a4c69d6cb94912a3ce16599b2d
vinitus <tlsdnrng@gmail.com>
2023-08-03 15:49:22 +0900

    재밌다 웹팩

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 const App = () => {
   return (
     <div>
-      <h1>Hello, React App with TypeScript and Webpack!</h1>
+      <h1>React + TypesSript 프로젝트 webpack으로 번들링하기</h1>
     </div>
   );
 };
