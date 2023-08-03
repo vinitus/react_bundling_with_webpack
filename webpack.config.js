23e2572c94c5cbd39a19b9571fe5e50af23392b5
vinitus <tlsdnrng@gmail.com>
2023-08-03 15:49:07 +0900

    html 플러그인 추가 및 주석 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
+// mjs와 cjs 중 무엇이 옳은지는 모르겠음..
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  // 진입점 설정
   entry: './src/index.tsx',
+  // 아웃풋 설정
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  // 타입 스크립트를 사용하는 경우, babel-loader를 사용하지 않아도 됨
   module: {
     rules: [
       {
@@ -17,7 +22,15 @@ module.exports = {
       },
     ],
   },
+  // resolve는 웹팩이 지정한 파일을 해석하는 방법을 지정하는 것이고
+  // extensions는 추가로 해석할 파일을 지정하는 것
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
   },
+  plugins: [
+    // filename이 index.html이라면 필요가 없음
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 };
